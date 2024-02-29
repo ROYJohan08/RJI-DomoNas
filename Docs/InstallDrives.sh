5961151c72d725e6af813a9566601c0729d1f9a7
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-02-29 08:37:35 +0100

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -1,3 +1,6 @@
-wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/fstab
-mv /etc/fstab /etc/fstab.old
-mv fstab /etc/fstab
+echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/Film auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/ArchiveFilm auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/6877284162D6D93C /media/Serie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/ArchiveSerie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
