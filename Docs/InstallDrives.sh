6dafa21ab42bcf00eb134bf6254d0d78ba47a0ff
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-04-08 21:41:28 +0200

    Add automount

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -10,3 +10,5 @@ echo "/dev/disk/by-uuid/6877284162D6D93C /media/Serie auto nosuid,nodev,nofail,x
 echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/ArchiveSerie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+
+sudo mount -a
