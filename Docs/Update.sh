cd600d921cfc1819f34eac8648092ccee042dbce
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-04-08 16:53:37 +0200

    Update Update.sh

--- Docs/Update.sh
+++ Docs/Update.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-wget https://raw.githubusercontent.com/ROYJohan08/DomotikHomeNas/main/Docs/smb.conf
-rm -rf /etc/samba/smb.conf.old
-mv /etc/samba/smb.conf /etc/samba/smb.conf.old
-mv smb.conf /etc/samba/smb.conf
+wget https://raw.githubusercontent.com/ROYJohan08/DomotikHomeNas/main/Docs/smb.conf # Get samba config.
+rm -rf /etc/samba/smb.conf.old # Remove oldest config
+mv /etc/samba/smb.conf /etc/samba/smb.conf.old # Save old config
+mv smb.conf /etc/samba/smb.conf #Set new config
+service smbd restart #Restart samba
 
-wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Docker.sh
-mkdir /etc/RJIDocker/
-rm -rf /etc/RJIDocker/Docker.sh
-mv Docker.sh /etc/RJIDocker/Docker.sh
+wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Docker.sh # Get Docker launcher
+mkdir /etc/RJIDocker/ # Create launcher folder
+rm -rf /etc/RJIDocker/Docker.sh # Remove old launcher
+mv Docker.sh /etc/RJIDocker/Docker.sh # Set the new Launcher
 
-rm -rf .bashrc.old
-mv .bashrc .bashrc.old
-wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/.bashrc
+rm -rf .bashrc.old # Remove Oldest BashRC
+mv .bashrc .bashrc.old# Save old bashRc
+wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/.bashrc #Get new bashrc
