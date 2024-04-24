21477f04c73ec7f08d554b2b3b18152837f88009
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-04-24 20:11:29 +0200

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -4,7 +4,7 @@ sudo mkdir /media/Serie
 sudo mkdir /media/Docs
 sudo mkdir /media/ArchiveSerie
 sudo mkdir /media/Runable
-echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/Film auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/OldSeries auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/ArchiveFilm auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/6877284162D6D93C /media/Serie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
