358ead4f9c502bab293d2ce21df3990765d930c5
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-11-22 21:26:18 +0100

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -1,14 +1,15 @@
 sudo mkdir /media/Film
-sudo mkdir /media/ArchiveFilm
+sudo mkdir /media/OldSeries
 sudo mkdir /media/Serie
 sudo mkdir /media/Docs
 sudo mkdir /media/ArchiveSerie
 sudo mkdir /media/Runable
+sudo mkdir /media/Runable2
 echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/OldSeries auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/ArchiveFilm auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Film auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/6877284162D6D93C /media/Serie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/ArchiveSerie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Runable2 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 
 sudo mount -a
