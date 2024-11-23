8466b67bb5d8d65d470308a41ef6002fce50b393
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-11-23 20:45:38 +0100

    Update Docker.sh

--- Docs/Docker.sh
+++ Docs/Docker.sh
@@ -2,40 +2,55 @@
 
 source /etc/RJIDocker/credentials.sh
 
-PortLM=80
-PortHA=1000
-PortSB=1001
-PortDB=1002
-PortJF=1003
-PortNC=1004
-PortFB=1005
-PortMQ=1007
+PortLM=80 # Lamp poer
+PortHA=1000 # HomeAssistant port
+PortJF=1001 # JellyFin port
+PortFB=1002 # FileBrowser port
+PortNC=1003 # NextCloud port
+PortPO=1004 # Portainer port
+PortSB=1005 # SeedBox Port 
+PortDB=1006 # DownBox Port
+PortMQ=1007 # MQTT Port
 
-
-
+ConfigLM="/media/Runable/Docker/LM-Config/"
 ConfigHA="/media/Runable/Docker/HA-Config"
+ConfigJF="/media/Runable/Docker/JF-Config"
+ConfigFB="/media/Runable/Docker/FB-Config/"
+ConfigNC="/media/Runable/Docker/NC-Config"
+ConfigPO="/media/Runable/Docker/PO-Config"
 ConfigSB="/media/Runable/Docker/SB-Config"
 ConfigDB="/media/Runable/Docker/DB-Config"
 Config2DB="/media/Runable/Docker/DB-Config/custom"
-ConfigJF="/media/Runable/Docker/JF-Config"
-ConfigNC="/media/Runable/Docker/NC-Config"
-ConfigFB="/media/Runable/Docker/FB-Config/"
-ConfigLM="/media/Runable/Docker/LM-Config/"
 ConfigMQ="/media/Runable/Docker/MQ-Config/"
 
+DataLM="/media/Runable/Docker/LM-Data"
+DataJF="/media/"
+DataFB="/"
+DataNC="/media/Docs/NC-Data"
 DataSB="/media/Runable/SeedBox"
 DataDB="/media/Runable/DownBox"
-DataJF="/media/"
-DataNC="/media/Docs/NC-Data"
-DataFB="/"
-DataLM="/media/Runable/Docker/LM-Data"
 DataMQ="/media/Runable/Docker/MQ-Data"
 
 case $1 in
 	"init")
 		case $2 in
-			"homeassistant")
-				sudo docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=CET -v $ConfigHA:/config -p $PortHA:8123 homeassistant/home-assistant:latest
+			"lamp")
+				sudo docker run -d --name lamp --restart=unless-stopped -e TZ=CET -v $DataLM:/var/www/html -p $PortLM:80 -p 3306:3306 lioshi/lamp:php7
+			;;
+   			"homeassistant")
+				sudo docker run -d --init --name homeassistant --privileged --restart=unless-stopped -e TZ=CET -v $ConfigHA:/config -p $PortHA:8123 homeassistant/home-assistant:stable
+			;;
+			"jellyfin")
+				sudo docker run -d --name jellyfin --restart=unless-stopped -e TZ=CET -v $ConfigJF:/config -v $DataJF:/media -p $PortJF:8096 -p 8920:8920 linuxserver/jellyfin:10.8.13
+			;;
+			"filebrowser")
+				sudo docker run -d --name filebrowser --restart=unless-stopped -e TZ=CET -v $DataFB:/srv -v $ConfigFB:/config/ -p $PortFB:80 filebrowser/filebrowser:v1.10.0
+			;;
+			"nextcloud")
+				sudo docker run -d --name nextcloud --restart=unless-stopped -e TZ=CET -p $PortNC:80 -v $ConfigNC:/var/www/html/config -v $DataNC:/data nextcloud:18.0.4
+			;;
+			"portainer")
+				docker run -d --name portainer --restart=unless-stopped -p 8000:8000 -p 9443:9443 -p $PortPO:9000 -v /var/run/docker.sock:/var/run/docker.sock -v $DataPO:/data portainer/portainer-ce:2.21.4
 			;;
 			"seedbox")
 				sudo git clone https://github.com/ronggang/transmission-web-control.git
@@ -52,18 +67,6 @@ case $1 in
 			"downbox")
 				sudo docker run -d --name transmission --privileged --restart=unless-stopped -p 9091:9091  -p 51415:51414 -p 51415:51414/udp --cap-add=NET_ADMIN -e TRANSMISSION_WEB_UI=transmission-web-control -v $Config2DB:/etc/openvpn/custom -v $DataDB:/data -v $ConfigDB:/config -e OPENVPN_PROVIDER=CUSTOM -e OPENVPN_USERNAME=$VPNUser -e OPENVPN_PASSWORD=$VPNPass -e UFW_ALLOW_GW_NET=true -e UFW_EXTRA_PORTS=9910,23561,443,83,9091 -e DROP_DEFAULT_ROUTE=true -e TRANSMISSION_RPC_USERNAME="$User" -e TRANSMISSION_RPC_PASSWORD="$Pass" -e TRANSMISSION_RPC_AUTHENTICATION_REQUIRED=true -e TRANSMISSION_RPC_WHITELIST_ENABLED=false -e LOCAL_NETWORK=192.168.1.0/32 --log-driver json-file --log-opt max-size=10m haugene/transmission-openvpn:3.6
 				sudo docker run -d --name downboxproxy --privileged --restart=unless-stopped --link transmission -p $PortDB:8080 haugene/transmission-openvpn-proxy
-			;;
-			"jellyfin")
-				sudo docker run -d --name jellyfin --restart=unless-stopped -e TZ=CET -v $ConfigJF:/config -v $DataJF:/media -p $PortJF:8096 -p 8920:8920 linuxserver/jellyfin:10.8.13
-			;;
-			"nextcloud")
-				sudo docker run -d --name nextcloud --restart=unless-stopped -e TZ=CET -p $PortNC:80 -v $ConfigNC:/var/www/html/config -v $DataNC:/data nextcloud:18.0.4
-			;;
-			"lamp")
-				sudo docker run -d --name lamp --restart=unless-stopped -e TZ=CET -v $DataLM:/var/www/html -p $PortLM:80 -p 3306:3306 lioshi/lamp:php7
-			;;
-			"filebrowser")
-				sudo docker run -d --name filebrowser --restart=unless-stopped -e TZ=CET -v $DataFB:/srv -v $ConfigFB:/config/ -p $PortFB:80 filebrowser/filebrowser:v1.10.0
 			;;
 			"mqtt")
 				sudo docker run -d --name mqtt --restart=unless-stopped -e TZ=CET -v $ConfigMQ:/mosquitto/config -v $DataMQ:/mosquitto/data -p $PortMQ:1883 -p 9001:9001  eclipse-mosquitto:2.0
