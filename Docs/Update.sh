22dbab9d94ae1bbf8ce7cce37f979a1b5a23d0f0
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2024-12-05 08:41:52 +0100

    Update Update.sh

--- Docs/Update.sh
+++ Docs/Update.sh
@@ -7,25 +7,33 @@ mv smb.conf /etc/samba/smb.conf # Set new config
 service smbd restart # Restart samba
 
 mkdir /etc/RJIDocker/ # Create launcher folder
-rm -rf /etc/RJIDocker/Docker.sh # Remove old launcher
+
+rm -rf /etc/RJIDocker/Docker.sh.old # Remove oldest launcher
 mv /etc/RJIDocker/Docker.sh /etc/RJIDocker/Docker.sh.old # Save old config.
 wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Docker.sh # Get Docker launcher
 mv Docker.sh /etc/RJIDocker/Docker.sh # Set the new Launcher
 
-rm -rf .bashrc.old # Remove Oldest BashRC
-mv .bashrc .bashrc.old # Save old bashRc
+rm -rf ~/.bashrc.old # Remove Oldest BashRC
+mv ~/.bashrc ~/.bashrc.old # Save old bashRc
 wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/.bashrc # Get new bashrc
-source .bashrc # Restart alias.
+mv .bashrc ~/.bashrc
+source ~/.bashrc # Restart alias.
 
-rm -rf Update.sh.old # Remove Oldest Update
+rm -rf /etc/RJIDocker/Update.sh.old # Remove Oldest Update
 mv /etc/RJIDocker/Update.sh /etc/RJIDocker/Update.sh.old # Save old Update
 wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Update.sh #Get new Update
 mv Update.sh /etc/RJIDocker/Update.sh # Move to folder
 
-rm -rf InstallDrives.sh.old # Remove Oldest Update
-mv InstallDrives.sh InstallDrives.sh.old # Save old Update
+rm -rf /etc/RJIDocker/InstallDrives.sh.old # Remove Oldest Update
+mv /etc/RJIDocker/InstallDrives.sh /etc/RJIDocker/InstallDrives.sh.old # Save old Update
 wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/InstallDrives.sh #Get new Update
+mv InstallDrives.sh /etc/RJIDocker/InstallDrives.sh
 
 wget https://github.com/ROYJohan08/RJI-DomoNas/raw/main/Docs/mycron # Get Crontab
 sudo crontab mycron # Set Crontab into crontab
 rm mycron # Remove temp crontab
+
+rm -rf /etc/RJIDocker/TvShowStorage.py.old # Remove Oldest Update
+mv /etc/RJIDocker/TvShowStorage.py /etc/RJIDocker/TvShowStorage.py.old # Save old Update
+wget https://github.com/ROYJohan08/RJI-DomoNas/blob/main/Docs/TvShowStorage.py #Get new Update
+mv TvShowStorage.py /etc/RJIDocker/TvShowStorage.py
