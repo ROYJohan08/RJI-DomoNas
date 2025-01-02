2865f0c3f9742628fe94fdf9b2571b758b0362da
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-01-02 17:44:45 +0100

    Update Update.sh

--- Docs/Update.sh
+++ Docs/Update.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
-#Version 202501021737
+#Version 202501021744
 
 #Save the version of the Update
 touch /etc/RJIDocker/Version.ver
-echo "#Version 202501021737" > /etc/RJIDocker/Version.ver
+echo "#Version 202501021744" > /etc/RJIDocker/Version.ver
+
+mkdir /etc/RJIDocker/ # Create launcher folder
+mkdir /etc/RJIDocker/old # Create launcher folder
 
 #UPDATE /etc/RJIDocker/Update_suite.sh
-rm -rf /etc/RJIDocker/Update_suite.sh.old #Remove Oldest Update.sh
-mv /etc/RJIDocker/Update_suite.sh /etc/RJIDocker/Update_suite.sh.old #Save old Update.sh
+rm -rf /etc/RJIDocker/old/Update_suite.sh.old #Remove Oldest Update.sh
+mv /etc/RJIDocker/Update_suite.sh /etc/RJIDocker/old/Update_suite.sh.old #Save old Update.sh
 wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Update_suite.sh #Get new Update.sh
 mv Update_suite.sh /etc/RJIDocker/Update_suite.sh #Move Udate.sh to the good directory
 bash /etc/RJIDocker/Update_suite.sh &
