975b877d997a1ec285f2afcaad45d65409941f55
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-01-02 17:31:44 +0100

    Update Update.sh

--- Docs/Update.sh
+++ Docs/Update.sh
@@ -7,6 +7,6 @@ echo "2501020002" > /etc/RJIDocker/Version.ver
 #UPDATE /etc/RJIDocker/Update_suite.sh
 rm -rf /etc/RJIDocker/Update_suite.sh.old #Remove Oldest Update.sh
 mv /etc/RJIDocker/Update_suite.sh /etc/RJIDocker/Update_suite.sh.old #Save old Update.sh
-wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Update_suite.sh > /dev/null #Get new Update.sh
+wget https://github.com/ROYJohan08/DomotikHomeNas/raw/main/Docs/Update_suite.sh #Get new Update.sh
 mv Update_suite.sh /etc/RJIDocker/Update_suite.sh #Move Udate.sh to the good directory
 bash /etc/RJIDocker/Update_suite.sh &
