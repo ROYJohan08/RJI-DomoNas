6913014f2165561c7585f563b3f598e68b2ad701
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-03-31 11:04:41 +0200

    250331-1104
    • Update Disk names

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -1,16 +1,16 @@
-sudo mkdir /media/Film
-sudo mkdir /media/OldSeries
-sudo mkdir /media/Serie
-sudo mkdir /media/Docs
-sudo mkdir /media/ArchiveSerie
+sudo mkdir /media/Films01
+sudo mkdir /media/Films02
+sudo mkdir /media/Series01
+sudo mkdir /media/Series02
+sudo mkdir /media/Series03
+sudo mkdir /media/Docs01
 sudo mkdir /media/Runable
-sudo mkdir /media/Runable2
-echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/OldSeries auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Film auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/6877284162D6D93C /media/Serie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Documents auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/ArchiveSerie auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Runable2 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/Series01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/Series03 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Films02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 echo "/dev/disk/by-uuid/F6809B44809B09EF /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
 
 sudo mount -a
