b62f4cb945e7c60336460f72a5bdd91db95112c1
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-04-24 16:02:19 +0200

    Update Docker.sh

--- Docs/Docker.sh
+++ Docs/Docker.sh
@@ -43,7 +43,7 @@ case $1 in
 				sudo docker run -d --name lamp --restart=unless-stopped -e TZ=CET -v $DataLM:/var/www/html -p $PortLM:80 -p 3306:3306 lioshi/lamp:php7
 			;;
    			"homeassistant")
-				sudo docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=CET -v $ConfigHA:/config -p 6666:6666 -p 6667:6667 -p $PortHA:8123 homeassistant/home-assistant:stable
+				sudo docker run -d --name homeassistant --privileged --restart=unless-stopped -e TZ=CET -v $ConfigHA:/config -p 6666:6666 -p 6667:6667 -p $PortHA:8123 homeassistant/home-assistant:latest
 			;;
 			"jellyfin")
 				sudo docker run -d --name jellyfin --restart=unless-stopped -e TZ=CET -v $ConfigJF:/config -v $DataJF:/media -p $PortJF:8096 -p 8920:8920 linuxserver/jellyfin:nightly
