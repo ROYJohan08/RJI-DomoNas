114880cdeb58038dceb2df42653b82e626541ccf
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-07-25 17:44:55 +0200

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -5,23 +5,16 @@ sudo mkdir /media/Series02
 sudo mkdir /media/Series03
 sudo mkdir /media/Docs01
 sudo mkdir /media/Runable
-#echo "/dev/disk/by-uuid/04C4848478981CC7 /media/Films02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/Series03 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/Series01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/F6809B44809B09EF /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-
-echo "/dev/disk/by-uuid/569B-EA54 /boot/efi vfat defaults 0 1" >> /etc/fstab
-echo "/dev/sdd1 /media/Series01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-#echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/sde1 /media/Series03 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/sdg1 /media/Films01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/sdc1 /media/Films02 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/sda1 /media/Runable auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
-echo "/dev/sdf1 /media/Docs01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab
+sudo mkdir /media/Temp
+sudo mkdir /media/Archive
 
 
-#echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs01 auto nosuid,nodev,nofail,x-gvfs-show 0 0" >> /etc/fstab #Disk HS temp.
+#echo "/dev/disk/by-uuid/462BA08F23A7BB61 /media/Series01 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/Series02 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series03 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/E8D250EAD250BF0E /media/Films02 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs01 auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/F6809B44809B09EF /media/Runable auto noatime 0 1" >> /etc/fstab
 
 sudo mount -a
