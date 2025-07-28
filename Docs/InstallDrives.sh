040ef8602d4d7116a2593beded2ef52a541bfe98
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-07-28 08:49:17 +0200

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -15,7 +15,7 @@ echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series03 auto noatime 0 1" >> /e
 echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto noatime 0 1" >> /etc/fstab
 echo "/dev/disk/by-uuid/E8D250EAD250BF0E /media/Films02 auto noatime 0 1" >> /etc/fstab
 echo "/dev/disk/by-uuid/2FB75EE05993C324 /media/Docs01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/F6809B44809B09EF /media/Runable auto noatime 0 1" >> /etc/fstab
+echo "/dev/disk/by-uuid/E8DA9EC8DA9E9304 /media/Runable auto noatime 0 1" >> /etc/fstab
 echo "/dev/disk/by-uuid/294D561D3EF1F925 /media/Archive auto noatime 0 1" >> /etc/fstab
 
 sudo mount -a --onlyonce
