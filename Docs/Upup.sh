fc8d411613af2461064500c569216ccb4ad8e672
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-08-05 13:02:16 +0200

    Delete Docs/Upup.sh

--- Docs/Upup.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /etc/RJIDomoNas/Old/Update.sh
-mv /etc/RJIDomoNas/Update.sh /etc/RJIDomoNas/Old/Update.sh
-wget https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Update.sh
-mv Update.sh /etc/RJIDomoNas/Update.sh
