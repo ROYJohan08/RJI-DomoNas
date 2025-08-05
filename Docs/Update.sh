bb21b05faeaf915018b3420e508decbc9c6efecf
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-08-05 14:01:37 +0200

    Update Update.sh

--- Docs/Update.sh
+++ Docs/Update.sh
@@ -21,8 +21,7 @@ mkdir /etc/RJIDomoNas/Old/ > /dev/null
 service smbd sto
 rm -rf /etc/samba/smb.conf.*
 mv -f /etc/samba/smb.conf /etc/samba/smb.conf.old
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/smb.conf
-mv -f ./smb.conf /etc/samba/smb.conf
+wget --output-document /etc/samba/smb.conf https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/smb.conf
 service smbd start
 
 ##################################################
@@ -31,8 +30,7 @@ service smbd start
 
 rm -rf /etc/RJIDomoNas/Old/Docker.sh
 mv -f /etc/RJIDomoNas/Docker.sh /etc/RJIDomoNas/Old/Docker.sh.
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Docker.sh
-mv -f ./Docker.sh /etc/RJIDomoNas/Docker.sh
+wget --output-document /etc/RJIDomoNas/Docker.sh https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Docker.sh
 
 ##################################################
 #                 Update  .bashrc                #
@@ -42,8 +40,7 @@ rm -rf /home/royjohan/.bashrc.*
 rm -rf /root/.bashrc.*
 cp /home/royjohan/.bashrc /home/royjohan/.bashrc.old
 cp /root/.bashrc /root/.bashrc.old
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/.bashrc
-yes | cp -rf .bashrc /home/royjohan/.bashrc
+wget --output-document /home/royjohan/.bashrc https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/.bashrc
 yes | cp -rf /home/royjohan/.bashrc /root/.bashrc
 source /root/.bashrc
 source /home/royjohan/.bashrc
@@ -54,8 +51,7 @@ source /home/royjohan/.bashrc
 
 rm -rf /etc/RJIDomoNas/Old/InstallDrives.sh
 mv -f /etc/RJIDomoNas/InstallDrives.sh /etc/RJIDomoNas/Old/InstallDrives.sh
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/InstallDrives.sh
-mv ./InstallDrives.sh /etc/RJIDomoNas/InstallDrives.sh
+wget --output-document /etc/RJIDomoNas/InstallDrives.sh https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/InstallDrives.sh
 
 ##################################################
 #                  Update mycron                 #
@@ -63,8 +59,7 @@ mv ./InstallDrives.sh /etc/RJIDomoNas/InstallDrives.sh
 
 rm -rf /etc/RJIDomoNas/Old/mycron
 mv -f /etc/RJIDomoNas/mycron /etc/RJIDomoNas/Old/mycron
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/mycron
-mv -f ./mycron /etc/RJIDomoNas/mycron
+wget --output-document /etc/RJIDomoNas/mycron https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/mycron
 sudo crontab /etc/RJIDomoNas/mycron
 
 ##################################################
@@ -73,8 +68,7 @@ sudo crontab /etc/RJIDomoNas/mycron
 
 rm -rf /etc/RJIDomoNas/Old/Archive.sh
 mv -f /etc/RJIDomoNas/Archive.sh /etc/RJIDomoNas/Old/Archive.sh
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Archive.sh
-mv -f ./Archive.sh /etc/RJIDomoNas/Archive.sh
+wget --output-document /etc/RJIDomoNas/Archive.sh https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Archive.sh
 
 ##################################################
 #              Update Renamer.sh                 #
@@ -82,8 +76,7 @@ mv -f ./Archive.sh /etc/RJIDomoNas/Archive.sh
 
 rm -rf /etc/RJIDomoNas/Old/Renamer.sh
 mv -f /etc/RJIDomoNas/Renamer.sh /etc/RJIDomoNas/Old/Renamer.sh
-wget -r https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Renamer.sh
-mv -f ./Renamer.sh /etc/RJIDomoNas/Renamer.sh
+wget --output-document /etc/RJIDomoNas/Renamer.sh https://raw.githubusercontent.com/ROYJohan08/RJI-DomoNas/refs/heads/main/Docs/Renamer.sh
 
 ##################################################
 #    @Date : 05/08/2025 12:57                    #
