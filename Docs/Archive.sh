ae4abab48927cf332c9808b9f6059ee80dc5cd34
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-08-05 12:36:12 +0200

    Update Archive.sh
    • Change method for simplify
    • Add document archive selection

--- Docs/Archive.sh
+++ Docs/Archive.sh
@@ -1,79 +1,45 @@
 #!bin/sh
+
 if mount | awk '{if ($3 == "/media/Archive") { exit 0}} ENDFILE{exit -1}'
 then
-    echo "Check if Archive is mounted : OK"
+
+    ##################################################
+    #                  Create folders                #
+    ##################################################
     
+    mkdir /media/Archive/Runable/ >/dev/null
+    mkdir /media/Archive/Docker/ >/dev/null
+    mkdir /media/Archive/Films/ >/dev/null
+    mkdir /media/Archive/Series/ >/dev/null
+    mkdir /media/Archive/Docs/ >/dev/null
+    mkdir /media/Archive/Docs/"+18"/ >/dev/null
+    mkdir /media/Archive/Docs/Musiques/ >/dev/null
+    mkdir /media/Archive/Docs/Photographies/ >/dev/null
+    mkdir /media/Archive/Docs/Livres/ >/dev/null
+    mkdir /media/Archive/Docs/Jeux/ >/dev/null
 	
-	
-    if ls "/media/Archive/" | awk '{if ($1 == "Runable") { exit 0}} ENDFILE{exit -1}'
-    then
-        echo "Check if Archive/Runable exist : OK"
-     else
-        echo "Check if Archive/Runable exist : KO"
-		mkdir /media/Archive/Runable/
-        echo "Creation of Archive/Runable : OK"
-    fi
-	if ls "/media/Archive/" | awk '{if ($1 == "Films") { exit 0}} ENDFILE{exit -1}'
-    then
-        echo "Check if Archive/Films exist : OK"
-     else
-        echo "Check if Archive/Films exist : KO"
-		mkdir /media/Archive/Films/
-        echo "Creation of Archive/Films : OK"
-    fi
-	if ls "/media/Archive/" | awk '{if ($1 == "Series") { exit 0}} ENDFILE{exit -1}'
-    then
-        echo "Check if Archive/Series exist : OK"
-     else
-        echo "Check if Archive/Series exist : KO"
-		mkdir /media/Archive/Series/
-        echo "Creation of Archive/Series : OK"
-    fi
-	if ls "/media/Archive/" | awk '{if ($1 == "Docs") { exit 0}} ENDFILE{exit -1}'
-    then
-        echo "Check if Archive/Docs exist : OK"
-     else
-        echo "Check if Archive/Docs exist : KO"
-		mkdir /media/Archive/Docs/
-        echo "Creation of Archive/Docs : OK"
-    fi
-	if ls "/media/Archive/" | awk '{if ($1 == "Runable") { exit 0}} ENDFILE{exit -1}'
-    then
-	
-        if ls "/media/Archive/Runable/" | awk '{if ($1 == "Docker") { exit 0}} ENDFILE{exit -1}'
-		then
-			echo "Check if Archive/Runable/Docker exist : OK"
-		else
-			echo "Check if Archive/Runable/Docker exist : KO"
-			mkdir /media/Archive/Runable/Docker/
-			echo "Creation of Archive/Runable/Docker : OK"
-		fi
-		if ls "/media/Archive/Runable/" | awk '{if ($1 == "SeedBox") { exit 0}} ENDFILE{exit -1}'
-		then
-			echo "Check if Archive/Runable/SeedBox exist : OK"
-		else
-			echo "Check if Archive/Runable/SeedBox exist : KO"
-			mkdir /media/Archive/Runable/SeedBox/
-			echo "Creation of Archive/Runable/SeedBox : OK"
-		fi
-     else
-        echo "Check if Archive/Runable exist : KO"
-		exit 1
-    fi
+    ##################################################
+    #                  Archive datas                 #
+    ##################################################
     
-	
-	
 	cp -a -d -f -R -u -v /media/Runable/Docker/* /media/Archive/Runable/Docker/ &
 	cp -a -d -f -R -u -v /media/Runable/SeedBox/* /media/Archive/Runable/SeedBox/ &
-	cp -a -d -f -R -u -v /media/Series01/A-*/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Series02/A-*/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Series03/A-*/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Series03/A-*/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Films01/A-* /media/Archive/Films/ &
-	cp -a -d -f -R -u -v /media/Films01/*/A-* /media/Archive/Films/ &
-	cp -a -d -f -R -u -v /media/Docs01/* /media/Archive/Docs/ &
+	cp -a -d -f -R -u -v /media/Series01/*-A/ /media/Archive/Series/ &
+	cp -a -d -f -R -u -v /media/Series02/*-A/ /media/Archive/Series/ &
+	cp -a -d -f -R -u -v /media/Series03/*-A/ /media/Archive/Series/ &
+	cp -a -d -f -R -u -v /media/Films01/*-A.* /media/Archive/Films/ &
+	cp -a -d -f -R -u -v /media/Films01/*/*-A.* /media/Archive/Films/ &
+	cp -a -d -f -R -u -v /media/Docs01/"+18"/* /media/Archive/Docs/"+18"/ &
+	cp -a -d -f -R -u -v /media/Docs01/Musiques/* /media/Archive/Docs/Musiques/ &
+	cp -a -d -f -R -u -v /media/Docs01/Photographies/* /media/Archive/Docs/Photographies/ &
+	cp -a -d -f -R -u -v /media/Docs01/Livres/* /media/Archive/Docs/Livres/ &
+	cp -a -d -f -R -u -v /media/Docs01/Jeux/*-A.* /media/Archive/Docs/Jeux/ &
 	
 else
-    echo "Check if Archive is mounted : KO"
-    echo "Exit"
 fi
+
+##################################################
+#    @Date : 05/08/2025 12:35                    #
+#    @Author : @ROYJohan                         #
+#    @Version : 10b                              #
+##################################################
