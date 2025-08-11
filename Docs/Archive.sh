cc35a6173eaaa5f143f90d19558bcf8387c505a7
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-08-11 08:37:07 +0200

    Update Archive.sh
    • Add datemarker
    • Remove multitask

--- Docs/Archive.sh
+++ Docs/Archive.sh
@@ -23,21 +23,23 @@ then
     ##################################################
     
 	# Copie intégrale de docker, seedbox. Pas de copie de downbox car facilement récupérable.
- 	cp -a -d -f -R -u -v /media/Runable/Docker/* /media/Archive/Runable/Docker/ &
-	cp -a -d -f -R -u -v /media/Runable/SeedBox/* /media/Archive/Runable/SeedBox/ &
+ 	cp -a -d -f -R -u -v /media/Runable/Docker/* /media/Archive/Runable/Docker/ 
+	cp -a -d -f -R -u -v /media/Runable/SeedBox/* /media/Archive/Runable/SeedBox/ 
  	# Copie de tout les classes -A des différents disques serie.
-	cp -a -d -f -R -u -v /media/Series01/*-A/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Series02/*-A/ /media/Archive/Series/ &
-	cp -a -d -f -R -u -v /media/Series03/*-A/ /media/Archive/Series/ &
+	cp -a -d -f -R -u -v /media/Series01/*-A/ /media/Archive/Series/ 
+	cp -a -d -f -R -u -v /media/Series02/*-A/ /media/Archive/Series/ 
+	cp -a -d -f -R -u -v /media/Series03/*-A/ /media/Archive/Series/ 
  	# Copie de tout les classes -A des différents disques Films.
-	cp -a -d -f -R -u -v /media/Films01/*-A.* /media/Archive/Films/ &
-	cp -a -d -f -R -u -v /media/Films01/*/*-A.* /media/Archive/Films/ &
-	cp -a -d -f -R -u -v /media/Docs01/"+18"/* /media/Archive/Docs/"+18"/ &
-	cp -a -d -f -R -u -v /media/Docs01/Musiques/* /media/Archive/Docs/Musiques/ &
-	cp -a -d -f -R -u -v /media/Docs01/Photographies/* /media/Archive/Docs/Photographies/ &
-	cp -a -d -f -R -u -v /media/Docs01/Livres/* /media/Archive/Docs/Livres/ &
-	cp -a -d -f -R -u -v /media/Docs01/Jeux/*-A.* /media/Archive/Docs/Jeux/ &
-	
+	cp -a -d -f -R -u -v /media/Films01/*-A.* /media/Archive/Films/ 
+	cp -a -d -f -R -u -v /media/Films01/*/*-A.* /media/Archive/Films/ 
+	cp -a -d -f -R -u -v /media/Docs01/"+18"/* /media/Archive/Docs/"+18"/ 
+	cp -a -d -f -R -u -v /media/Docs01/Musiques/* /media/Archive/Docs/Musiques/ 
+	cp -a -d -f -R -u -v /media/Docs01/Photographies/* /media/Archive/Docs/Photographies/ 
+	cp -a -d -f -R -u -v /media/Docs01/Livres/* /media/Archive/Docs/Livres/ 
+	cp -a -d -f -R -u -v /media/Docs01/Jeux/*-A.* /media/Archive/Docs/Jeux/ 
+
+ 	echo date "+%d/%m/%y %H:%M:%S" > /media/Archive/LastArchive.dt
+ 	
 else
 fi
 
