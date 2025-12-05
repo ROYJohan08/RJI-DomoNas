15285967efb244fef1b7ef62d3ed3822396ae33f
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-12-05 12:50:18 +0100

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -1,30 +1,75 @@
-#!bin/sh
+#!/bin/sh
 
-sudo mkdir /media/Films01
-sudo mkdir /media/Films02
-sudo mkdir /media/Series01
-sudo mkdir /media/Series02
-sudo mkdir /media/Series03
-sudo mkdir /media/Docs01
-sudo mkdir /media/Runable
-sudo mkdir /media/Temp
-sudo mkdir /media/Archive
+# --------------------------------------------------
+# CRÉATION DES POINTS DE MONTAGE (ROBUSTE)
+# --------------------------------------------------
+mkdir -p /media/Films01 /media/Films02 /media/Series01 /media/Series02 /media/Series03 /media/Docs01 /media/Runable /media/Temp /media/Archive
 
+# --------------------------------------------------
+# ATTRIBUTION DES DROITS D'UTILISATION (Assurez-vous que l'utilisateur 'royjohan' a les droits)
+# --------------------------------------------------
+chown royjohan:royjohan /media/Films01 /media/Films02 /media/Series01 /media/Series02 /media/Series03 /media/Docs01 /media/Runable /media/Archive
 
-echo "/dev/disk/by-uuid/07C1B017487CD384 /media/Series01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/Series02 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series03 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/E8D250EAD250BF0E /media/Films02 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/1233D736318CD68C /media/Docs01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/00C52F7F6202AA50 /media/Runable auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/294D561D3EF1F925 /media/Archive auto noatime 0 1" >> /etc/fstab
+# --------------------------------------------------
+# AJOUT DANS /etc/fstab (Conditionnel : évite les doublons)
+# --------------------------------------------------
 
-sudo mount -a --onlyonce
+# /media/Series01
+UUID_SERIES1="07C1B017487CD384"
+if ! grep -q "$UUID_SERIES1" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_SERIES1 /media/Series01 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Series02
+UUID_SERIES2="0C2020846A4195D6"
+if ! grep -q "$UUID_SERIES2" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_SERIES2 /media/Series02 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Series03
+UUID_SERIES3="6877284162D6D93C"
+if ! grep -q "$UUID_SERIES3" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_SERIES3 /media/Series03 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Films01
+UUID_FILMS1="23BF5EEE4C02F6EE"
+if ! grep -q "$UUID_FILMS1" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_FILMS1 /media/Films01 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Films02
+UUID_FILMS2="E8D250EAD250BF0E"
+if ! grep -q "$UUID_FILMS2" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_FILMS2 /media/Films02 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Docs01
+UUID_DOCS1="1233D736318CD68C"
+if ! grep -q "$UUID_DOCS1" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_DOCS1 /media/Docs01 auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Runable
+UUID_RUNABLE="00C52F7F6202AA50"
+if ! grep -q "$UUID_RUNABLE" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_RUNABLE /media/Runable auto noatime 0 1" >> /etc/fstab
+fi
+
+# /media/Archive
+UUID_ARCHIVE="294D561D3EF1F925"
+if ! grep -q "$UUID_ARCHIVE" /etc/fstab; then
+    echo "/dev/disk/by-uuid/$UUID_ARCHIVE /media/Archive auto noatime 0 1" >> /etc/fstab
+fi
+
+# --------------------------------------------------
+# MONTAGE DES NOUVEAUX PÉRIPHÉRIQUES
+# --------------------------------------------------
+mount -a --onlyonce
 
 
 ##################################################
-#    @Date : 05/08/2025 12:52                    #
-#    @Author : @ROYJohan                         #
-#    @Version : 10b                              #
+# @Date : 05/12/2025 12:48
+# @Author : @ROYJohan
+# @Version : 12b
 ##################################################
