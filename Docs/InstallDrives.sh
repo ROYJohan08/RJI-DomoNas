292d82de2323e07c68f14348d40ca6bb0142083f
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-12-16 17:42:16 +0100

    Update InstallDrives.sh

--- Docs/InstallDrives.sh
+++ Docs/InstallDrives.sh
@@ -1,40 +1,48 @@
 #!/bin/sh
 
 # --------------------------------------------------
-# CRÉATION DES POINTS DE MONTAGE (ROBUSTE)
+# CRÉATION DES POINTS DE MONTAGE
 # --------------------------------------------------
-mkdir /media/Films01 
-mkdir /media/Films02
-mkdir/media/Series01
-mkdir/media/Series02
-mkdir/media/Series03
-mkdir /media/Docs01 
-mkdir /media/Runable
-mkdir /media/Temp 
+# Utilisation de mkdir -p pour éviter les erreurs si le dossier existe déjà
+mkdir -p /media/Films01 /media/Films02 /media/Series01 /media/Series02 /media/Series03 /media/Docs01 /media/Runable /media/Temp /media/Archive
 
+# --------------------------------------------------
+# AJOUT DANS /etc/fstab
+# --------------------------------------------------
+# Note : On ajoute 'nofail' pour éviter que le PC bloque au démarrage si un disque est débranché.
+# On ajoute 'umask=000' pour donner les droits 777 (tout le monde) automatiquement au montage.
+
+# Fonction pour ajouter proprement au fstab sans doublons (optionnel mais conseillé)
+add_to_fstab() {
+    UUID=$1
+    MOUNT_POINT=$2
+    if ! grep -qs "$UUID" /etc/fstab; then
+        echo "UUID=$UUID $MOUNT_POINT auto nosuid,nodev,nofail,xtime,umask=000 0 0" >> /etc/fstab
+    fi
+}
+
+add_to_fstab "07C1B017487CD384" "/media/Series01"
+add_to_fstab "0C2020846A4195D6" "/media/Series02"
+add_to_fstab "6877284162D6D93C" "/media/Series03"
+add_to_fstab "23BF5EEE4C02F6EE" "/media/Films01"
+add_to_fstab "E8D250EAD250BF0E" "/media/Films02"
+add_to_fstab "1233D736318CD68C" "/media/Docs01"
+add_to_fstab "00C52F7F6202AA50" "/media/Runable"
+add_to_fstab "294D561D3EF1F925" "/media/Archive"
+
+# --------------------------------------------------
+# MONTAGE ET DROITS FINAUX
+# --------------------------------------------------
+mount -a
+
+# On réapplique les droits après le montage pour les systèmes de fichiers Linux (ext4)
 chmod -R 777 /media/
+chown -R royjohan:royjohan /media/
 
-# --------------------------------------------------
-# AJOUT DANS /etc/fstab (Conditionnel : évite les doublons)
-# --------------------------------------------------
-
-echo "/dev/disk/by-uuid/07C1B017487CD384 /media/Series01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/0C2020846A4195D6 /media/Series02 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/6877284162D6D93C /media/Series03 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/23BF5EEE4C02F6EE /media/Films01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/E8D250EAD250BF0E /media/Films02 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/1233D736318CD68C /media/Docs01 auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/00C52F7F6202AA50 /media/Runable auto noatime 0 1" >> /etc/fstab
-echo "/dev/disk/by-uuid/294D561D3EF1F925 /media/Archive auto noatime 0 1" >> /etc/fstab
-
-# --------------------------------------------------
-# MONTAGE DES NOUVEAUX PÉRIPHÉRIQUES
-# --------------------------------------------------
-mount -a --onlyonce
-
+echo "Disques montés avec accès universel."
 
 ##################################################
 # @Date : 05/12/2025 12:48
 # @Author : @ROYJohan
-# @Version : 12b
+# @Version : 12c (Updated for permissions)
 ##################################################
