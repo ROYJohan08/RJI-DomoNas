a64b06e1fa7b16c2555374428be5ab7817edc1ef
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2025-12-31 09:48:03 +0100

    Update Archive.sh

--- Docs/Archive.sh
+++ Docs/Archive.sh
@@ -3,63 +3,63 @@
 mount -a --onlyonce
 if mount | grep -q 'on /media/Archive '
 then
-	echo "Archivage ---- Creation des dossiers... "
+	echo "Archivage ---- Creation des dossiers... " >> /media/Archive/Log.dat
 	mkdir -p /media/Archive/Runable/Docker/ \
          /media/Archive/Films/ \
          /media/Archive/Series/ \
-         /media/Archive/Docs/"+18"/ \
+         /media/Archive/Docs/18/ \
          /media/Archive/Docs/Musiques/ \
          /media/Archive/Docs/Photographies/ \
          /media/Archive/Docs/Livres/ \
          /media/Archive/Docs/Jeux/ >/dev/null
-	echo "Archivage ---- Creation des dossiers... OK"
+	echo "Archivage ---- Creation des dossiers... OK" >> /media/Archive/Log.dat
 
-	echo "Archivage ---- Archivage des Docker..."
+	echo "Archivage ---- Archivage des Docker..." >> /media/Archive/Log.dat
  	cp -a -d -f -R -u -v /media/Runable/Docker/* /media/Archive/Runable/Docker/  >> /media/Archive/Log.dat
-  	echo "Archivage ---- Archivage des Docker...OK"
-   	echo "Archivage ---- Archivage de la SeedBox..."
+  	echo "Archivage ---- Archivage des Docker...OK" >> /media/Archive/Log.dat
+   	echo "Archivage ---- Archivage de la SeedBox..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Runable/DownBox/SeedBox/* /media/Archive/Runable/SeedBox/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage de la SeedBox...OK"
-  	echo "Archivage ---- Archivage des '-A' du disque Series01..."
+ 	echo "Archivage ---- Archivage de la SeedBox...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' du disque Series01..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Series01/*-A/ /media/Archive/Series/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' du disque Series01...OK"
-  	echo "Archivage ---- Archivage des '-A' du disque Series02..."
+ 	echo "Archivage ---- Archivage des '-A' du disque Series01...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' du disque Series02..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Series02/*-A/ /media/Archive/Series/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' du disque Series02...OK"
-  	echo "Archivage ---- Archivage des '-A' du disque Series03..."
+ 	echo "Archivage ---- Archivage des '-A' du disque Series02...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' du disque Series03..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Series03/*-A/ /media/Archive/Series/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' du disque Series03...OK"
-  	echo "Archivage ---- Archivage des '-A' du disque Film01..."
+ 	echo "Archivage ---- Archivage des '-A' du disque Series03...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' du disque Film01..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Films01/*-A.* /media/Archive/Films/  >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Films01/*/*-A.* /media/Archive/Films/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' du disque Film01...OK"
-  	echo "Archivage ---- Archivage des '-A' du disque Film02..."
+ 	echo "Archivage ---- Archivage des '-A' du disque Film01...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' du disque Film02..." >> /media/Archive/Log.dat
   	cp -a -d -f -R -u -v /media/Films02/*-A.* /media/Archive/Films/  >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Films02/*/*-A.* /media/Archive/Films/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' du disque Film02...OK"
-  	echo "Archivage ---- Archivage des documents +18..."
+ 	echo "Archivage ---- Archivage des '-A' du disque Film02...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des documents +18..." >> /media/Archive/Log.dat
 	
 	
 	
 	
 	
-	cp -a -d -f -R -u -v "/media/Docs01/+18/"* "/media/Archive/Docs/+18/"  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des documents +18...OK"
-  	echo "Archivage ---- Archivage des musiques..."
+	cp -a -d -f -R -u -v "/media/Docs01/18/"* "/media/Archive/Docs/18/"  >> /media/Archive/Log.dat
+ 	echo "Archivage ---- Archivage des documents +18...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des musiques..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Docs01/Musiques/* /media/Archive/Docs/Musiques/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des musiques...OK"
-  	echo "Archivage ---- Archivage des photos..."
+ 	echo "Archivage ---- Archivage des musiques...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des photos..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Docs01/Photographies/* /media/Archive/Docs/Photographies/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des photos...OK"
-  	echo "Archivage ---- Archivage des livres..."
+ 	echo "Archivage ---- Archivage des photos...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des livres..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Docs01/Livres/* /media/Archive/Docs/Livres/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des livres...OK"
-  	echo "Archivage ---- Archivage des '-A' des jeux..."
+ 	echo "Archivage ---- Archivage des livres...OK" >> /media/Archive/Log.dat
+  	echo "Archivage ---- Archivage des '-A' des jeux..." >> /media/Archive/Log.dat
 	cp -a -d -f -R -u -v /media/Docs01/Jeux/*-A.* /media/Archive/Docs/Jeux/  >> /media/Archive/Log.dat
- 	echo "Archivage ---- Archivage des '-A' des jeux...OK"
-	echo "Archivage ---- Srockage de la date"
+ 	echo "Archivage ---- Archivage des '-A' des jeux...OK" >> /media/Archive/Log.dat
+	echo "Archivage ---- Srockage de la date" >> /media/Archive/Log.dat
  	echo "$(date "+%d/%m/%y %H:%M:%S")" > /media/Archive/LastArchive.dt
-  	echo "Archivage ---- Archivage ---- FIN"
+  	echo "Archivage ---- Archivage ---- FIN" >> /media/Archive/Log.dat
 fi
 
 ##################################################
