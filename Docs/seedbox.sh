abdcce51cff2cb63a1701527ddba1cd8a7d7c36d
ROYJohan08 <117516857+ROYJohan08@users.noreply.github.com>
2026-01-01 20:37:25 +0100

    Fix + Adding logs

--- Docs/seedbox.sh
+++ Docs/seedbox.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-cp -r /data/DownBox/.seed/* /media/SeedBox/
-mv /data/DownBox/.seed/* /media/DownBox/
+echo "Copie des fichier de .seed dans la seedbox" >> /data/log.log
+cp -f -r -v /data/DownBox/.seed/* /data/SeedBox/ >> /dev/null
+
+echo "Deplacement des fichier de .seed vers Downbox" >> /data/log.log
+mv -f -v /data/DownBox/.seed/* /data/DownBox/ >> /data/log.log
+echo "Fin de script" >> /data/log.log
